56488832723923fff08228173bccb97e479cb27c
oliversaternus <mail@oliversaternus.de>
2021-05-31 00:04:35 +0200

    refactored backend

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http:localhost:1337'),
+  url: env('URL', 'http://localhost:1337'),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '06ad933b689f681fb23c0ce59be46707'),
+      secret: env('ADMIN_JWT_SECRET', '3de5b33be8b51e5a03ce210f436940bc'),
     },
   },
 });
